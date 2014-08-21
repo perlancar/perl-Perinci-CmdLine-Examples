89621ac72ff95ac9560bab47ca87fc0128151ff0
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-08-21 19:15:16 +0700

    Initial commit

---
+++ lib/Perinci/CmdLine/Examples.pm
@@ -0,0 +1,15 @@
+package Perinci::CmdLine::Examples;
+
+# DATE
+# ABSTRACT
+
+use 5.010001;
+
+1;
+# ABSTRACT: Examples for Perinci::CmdLine
+
+=head1 DESCRIPTION
+
+This distribution includes C<examples/> which contains example scripts that use
+L<Perinci::CmdLine>.
+
